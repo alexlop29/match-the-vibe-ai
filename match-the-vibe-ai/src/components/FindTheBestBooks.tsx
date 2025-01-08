c4d709018659e02800815112aa0a1a5773f8bf1f
alexlop29 <alexander.lopez@owasp.org>
2025-01-08 09:15:24 -0500

    Reconfig comps to use tanstack to trigger the OpenAI sample call

--- match-the-vibe-ai/src/components/FindTheBestBooks.tsx
+++ match-the-vibe-ai/src/components/FindTheBestBooks.tsx
@@ -1,22 +1,58 @@
+// default
+import { useState, useCallback } from "react";
+
+// deps
+import { useMutation } from "@tanstack/react-query";
+import { RecommendService } from "@/services/RecommendService";
+
 // comps
 import { Input } from "./ui/input";
 import { Button } from "./ui/button";
 
 const FindTheBestBooks = () => {
+  const [query, setQuery] = useState({
+    songName: "",
+    artistName: "",
+  });
+
+  const handleChange = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      const value = {
+        ...query,
+        [e.target.name]: e.target.value,
+      };
+      setQuery(value);
+    },
+    [query],
+  );
+
+  const { mutate: handleQuery } = useMutation({
+    mutationFn: async () => {
+      const recommendService = new RecommendService();
+      let recommendation = await recommendService.get();
+      console.log(recommendation);
+    },
+    onError: (error) => {
+      console.log(error);
+    },
+  });
+
   return (
     <div className="bg-white p-2 rounded-lg">
       <div className="flex flex-row justify-between bg-blue rounded-lg p-8 content-center">
         <div>Find the Best Books For You</div>
         <div>
           <div>Song Name</div>
-          <Input />
+          <Input onBlur={handleChange} name="songName" />
         </div>
         <div>
           <div>Artist Name</div>
-          <Input />
+          <Input onBlur={handleChange} name="artistName" />
         </div>
         <div>
-          <Button className="bg-purple">Try Now</Button>
+          <Button className="bg-purple" onClick={() => handleQuery()}>
+            Try Now
+          </Button>
         </div>
       </div>
     </div>
